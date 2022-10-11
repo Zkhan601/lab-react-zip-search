f13a775322bc356a935f3a22d596fe38fbc8ecbf
Zkhan601 <95313563+Zkhan601@users.noreply.github.com>
2022-10-11 16:46:16 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import React, {useState} from "react";
 import "./App.css";
 
 
-function City({ city, state, location, pop, wages }) {
+function City({ city, state, location,pops, totalWages }) {
 	return (
-		<div>
-			<div className="card mb-5">
-				<div className="card-header">{`${city}, ${state}`}</div>
-				<div className="card-body">
-					<ul>
-						<li>{`State: ${state}`}</li>
-						<li>{`Location: ${location}`}</li>
-						<li>{`Population (estimated): ${pop}`}</li>
-						<li>{`Total Wages: ${wages}`}</li>
-					</ul>
-				</div>
-			</div>
+	<div>
+	<div className="card mb-5">
+		<div className="card-header">{`${city}, ${state}`}</div>
+		<div className="card-body">
+			<ul>
+			  <li>{`State: ${state}`}</li>
+				<li>{`Location: ${location}`}</li>
+				<li>{`Population(estimated): ${pops}`}</li>
+				<li>{`Total Wages: ${totalWages}`}</li>
+			</ul>
 		</div>
+	</div>
+ </div>
 	);
 }
 
 function ZipSearchField(props) {
-	return (
-		<div className="my-5">
-			<label htmlFor="zip-code">Zip Code:</label>
-			<input
-				type="text"
-				className="form-control"
-				id="zip-code"
-				onChange={props.handleChange}
-			/>
-		</div>
-	);
+return (
+	<div className="my-5">
+		<label htmlFor="zip-code">Zip Code:</label>
+		<input
+			type="text"
+			className="form-control"
+			id="zip-code"
+			onChange={props.handleChange}
+		/>
+	</div>
+);
 }
 
 function App() {
@@ -47,37 +47,35 @@ function App() {
           .then((body) => setZipCodeResults(body))
         } else{
           setZipCodeResults([]);
-          console.log("invalid input");
-          <strong>Invalid</strong>;
         }      
   };
 
 	return (
 		<div className="App">
-			<div className="App-header">
-				<h1>Zip Code Search</h1>
-			</div>
-			<div className="mx-auto" style={{ maxWidth: 400 }}>
-				<ZipSearchField value = {zipCode} handleChange={getData} />
-				<div>
-					{zipCodeResults.map((data) => (
-							<City
-								key={data.RecordNumber}
-								city={data.City}
-								state={data.State}
-								location={`(${data.Lat}, ${data.Long})`}
-								estimatedPop={data.pop}
-								totalWages={data.wages}
-							/>
-						))}
-						<div>
-              {zipCodeResults.length < 5 && <bold>Not Found</bold>}
-						</div>
-				</div>
-			</div>
+		<div className="App-header">
+		<h1>Zip Code Search</h1>
 		</div>
+		 <div className="mx-auto" style={{ maxWidth: 400 }}>
+			 <ZipSearchField value = {zipCode} handleChange={getData} />
+			 <div>
+			 {zipCodeResults.map((data) => (
+				<City
+					key={data.RecordNumber}
+					city={data.City}
+					state={data.State}
+					location={`(${data.Lat}, ${data.Long})`}
+					pops={data.EstimatedPopulation}
+					totalWages={data.TotalWages}
+				/>
+	    ))}
+		<div>
+      {zipCodeResults.length < 5 && <bold>Not Found</bold>}
+		  </div>
+		 </div>
+		</div>
+	</div>
     
-    );
+  );
 }
 
 export default App;
